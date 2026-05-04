4e27375e9fbe7d597a7c3f2db93c9f68b9495e9d
Dev Singh <dev.singh@rakuten.com>
2026-05-04 17:28:31 +0900

    Initial Commit

---
+++ TinyGlow/TinyGlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyGlowApp.swift
+//  TinyGlow
+//
+//  Created by Singh, Dev | Dev | LPD on 2026/05/04.
+//
+
+import SwiftUI
+
+@main
+struct TinyGlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
